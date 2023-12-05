aaacfb2d7dd43cee908570d7d0d12fc393076689
aryadira <aryadyra27@gmail.com>
2023-12-05 21:38:08 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "blue-purple": "#3a10e5",
+        "blue-purple-hover": "#3d1eb9",
+        "blue-old": "#090e23",
+        "blue-young": "#f0f3ff",
+        "blue-young-2": "#66c4ff",
+        "yellow-benefit": "#facb3b",
+        "pink-custom": "#f966ff",
+        "beige-custom": "#fff0e5",
+        "slate-custom": "#f0f3ff",
+      },
+    },
+  },
+  plugins: [],
+};
